229b00358095888f115a5bee0c2d449279b678e7
Jeongmings <jeongmin.lee@elixirtopik.com>
2023-11-23 11:19:11 +0900

    nginx

--- core/core/urls.py
+++ core/core/urls.py
@@ -25,16 +25,16 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path("api/v1/user/register/", RegisterAPIView.as_view()),  # post - 회원가입
-    path("api/v1/user/auth/", UserLoginAPI.as_view()),
+    path("v1/user/register/", RegisterAPIView.as_view()),  # post - 회원가입
+    path("/v1/user/auth/", UserLoginAPI.as_view()),
     # post - 로그인, delete - 로그아웃, get - 유저정보
-    path("api/v1/", include("api.urls")),
+    path("v1/", include("api.urls")),
 
     # JWT Token auth
-    path('api/v1/auth/', TokenObtainPairView.as_view()),
-    path('api/v1/auth/refresh/', TokenRefreshView.as_view()),
-    path('api/v1/auth/verify/', TokenVerifyView.as_view()),
-    path('api/v1/auth/logout/', TokenBlacklistView.as_view()),
+    path('v1/auth/', TokenObtainPairView.as_view()),
+    path('v1/auth/refresh/', TokenRefreshView.as_view()),
+    path('v1/auth/verify/', TokenVerifyView.as_view()),
+    path('v1/auth/logout/', TokenBlacklistView.as_view()),
 
     # path('main', main_page, name="main"),
     # path('main/team', team_page, name="team"),
